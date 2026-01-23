22d763b810efe970289b8b4f1c5d36e8095146a7
kuangkai <kuangkai@kylinos.cn>
2026-01-23 14:29:31 +0800

    add spec file
    Signed-off-by: kuangkai <kuangkai@kylinos.cn>

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NAME="fgds"
+VERSION="1.0.0"
+BUILDTAG="HEAD"
+RELEASE=1
+RPMBUILD_TMPDIR=$(pwd)/tmp-rpmbuild
+
+function make_srpm() {
+  	[ -d ${RPMBUILD_TMPDIR} ] && rm -rf ${RPMBUILD_TMPDIR}
+  	mkdir -p ${RPMBUILD_TMPDIR}/{SOURCES,SPECS}
+  	git archive --format=tar.gz --prefix=${NAME}-${VERSION}/ --output=${RPMBUILD_TMPDIR}/SOURCES/${NAME}-${VERSION}.tar.gz ${BUILDTAG}
+  	sed -e "s/@VERSION@/${VERSION}/g" -e "s/@RELEASE@/${RELEASE}/g" fgds.spec.in > ${RPMBUILD_TMPDIR}/SPECS/fgds.spec
+  	rpmbuild -bs --define "_topdir ${RPMBUILD_TMPDIR}" ${RPMBUILD_TMPDIR}/SPECS/fgds.spec
+}
+
+make_srpm
